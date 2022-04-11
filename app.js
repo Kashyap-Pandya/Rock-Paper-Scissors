477fc29566238b7b7c774e5ff82b67e4b9fa1c57
Kashyap Pandya <17811129+Kashyap-Pandya@users.noreply.github.com>
2022-04-11 06:30:56 +0530

    [ADD]added fun to calculate who beats who
    e.g. rock beats paper - still have few bugs - i think. working on it

--- app.js
+++ app.js
@@ -14,5 +14,23 @@ computerPlay();
 // 
 
 function playRound(playerSelection, computerSelection) {
-    
+   if("rock" > "paper") {
+       console.log("rock beats paper");
+   }
+
+   else if ("paper" < "scissors") {
+        console.log("scissors beats paper");
+   }
+
+ else if ("rock" > "scissors") {
+       console.log("rock beats scissors")
+   }
+
+
 }
+
+playRound();
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection , computerSelection));
