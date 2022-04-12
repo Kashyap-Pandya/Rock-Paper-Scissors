de1b384c875b98bcfa586ebc35d8b05ff7d4542f
Kashyap Pandya <17811129+Kashyap-Pandya@users.noreply.github.com>
2022-04-13 02:37:01 +0530

    [IMP] better commented and formatted

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ let playerScore = 0;
 let computerScore = 0;
 
 function computerPlay() {
-    const randomChoice = Math.floor(Math.random() * choices.length);
-    const randomName = choices[randomChoice];
+    const randomChoice = Math.floor(Math.random() * choices.length);    //gets the  0,1,2 -randomly 
+    const randomName = choices[randomChoice];   // put the random number into an array, giving us random name
     return randomName;
     // console.log(randomName);
 }
@@ -53,55 +53,48 @@ function playRound(playerSelection, computerSelection) {
 }
 playRound();
 
-
-
-
-
 // let result =(playRound(playerSelection, computerSelection));
 // console.log(result);
 
 function game() {
-     for (let i = 1; i < 6; i++) {
+    for (let i = 1; i < 6; i++) {
         let userInput = prompt("what's your choice? rock-paper-scissors");
         let playerSelection = userInput.toLowerCase();
         console.log("game", i);
+
         let computerSelection = computerPlay();
         console.log(`you picked ${playerSelection}`);
-        let result =(playRound(playerSelection, computerSelection));
-        console.log(result);
 
-        if(result.includes("win")) {
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
+        
+        // calculating the score and displaying at each game
+
+        if (result.includes("win")) {
             playerScore++;
-    
-        }
-    
-        else if (result.includes("lose")) {
+        } else if (result.includes("lose")) {
             computerScore++;
-    
-        }
-    
-        else if (result.includes("tie")) {
+        } else if (result.includes("tie")) {
             playerScore;
             computerScore;
         }
-    
+
         console.log(`player score is ${playerScore}`);
         console.log(`computer score is ${computerScore}`);
     }
-            function winner() {
-                if (computerScore > playerScore) {
-                    console.log("computer wins, best of luck next time")
-                }
 
-                else if (playerScore > computerScore) {
-                    console.log("you're natural, suck it up AI")
-                }
+    // displaying winner after the 5 game is over
 
-                else if (playerScore == computerScore) {
-                    console.log("you both suck, try again")
-                }
-            }
-            winner();
-     }
-   
+    function winner() {
+        if (computerScore > playerScore) {
+            console.log("computer wins, best of luck next time");
+        } else if (playerScore > computerScore) {
+            console.log("you're natural, suck it up AI");
+        } else if (playerScore == computerScore) {
+            console.log("you both suck, try again");
+        }
+    }
+    winner();
+}
+
 game();
